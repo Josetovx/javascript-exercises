cad709b80896c1b4a246e7ef4c80a6fa60935b04
Josetovx <10379711+Josetovx@users.noreply.github.com>
2025-01-24 19:39:20 +0100

    Solve first item
    Now use a loop to solve the previous problem.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(text, times) {
-    return text.repeat(times);
+    let string="";
+    for (let i=0; i<times; i++) {
+        string +=text;
+    }
+    return string
 };
 
 repeatString("hey", 3)
