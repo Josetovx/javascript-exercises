f8d5bbfe56e6f3e048374b3af40f391b9f61871a
Josetovx <10379711+Josetovx@users.noreply.github.com>
2025-01-24 19:27:02 +0100

    Write repeat function
    Write a function for repeating a string.
    But it is not using loops, yet.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
-    let string =""
+const repeatString = function(text, times) {
+    return text.repeat(times);
 };
+
+repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
