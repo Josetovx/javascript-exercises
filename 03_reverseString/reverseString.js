fcda0199cacbf871c60ca84d9060b254d88bdcb9
Josetovx <10379711+Josetovx@users.noreply.github.com>
2025-01-25 16:55:18 +0100

    Transform the string
    To pass the test:
    - Convert the string to a split array.
    - Reverse the array.
    - Join the reverse array and convert it to a string again.
    
    Then pass all the tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
 const reverseString = function(text) {
-    let text=""
+    let splitArray = text.split("");
+    let reverseArray = splitArray.reverse();
+    let reverseJoined = reverseArray.join("");
+    return reverseJoined;
 };
 
 // Do not edit below this line
