f00cc6bd909b9394c126a56f967d3efafcc2095f
Josetovx <10379711+Josetovx@users.noreply.github.com>
2025-01-28 12:51:09 +0100

    Enter "if" condition
    Enter a condition to pass the test if the first number
    is larger than the second one.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(intBeg, intFin) {
+    if (intFin < intBeg) {
+        let change = intBeg;
+        intBeg = intFin;
+        intFin = change;
+    }
     let sum = 0;
     for (let i = intBeg; i <= intFin; i++) {
       sum += i
